583874e824c21ab4f948eb0f77610461fec848c9
= <=>
2024-06-06 12:28:49 +0300

    Scoring service testing adjustment (score coverage added)

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
-
+    //Точка старта программы
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
     }
